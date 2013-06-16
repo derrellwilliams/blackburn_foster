e2549de498467e468ce95db05475b4960405ba27
Matthew Williams <matthew@urbancompass.com>
2013-06-15 22:00:37 -0400

    updating theme

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Allan Glanfield - Menswear Designer</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<title><?php bloginfo(); ?></title>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo ( 'stylesheet_url' ); ?>">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?> webfonts/ss-social.css" type="text/css" media="screen"/>
 <script type="text/javascript" src="//use.typekit.net/gmj5rob.js"></script>
-<link rel="stylesheet" media="screen, projection" href="webfonts/ss-social.css" />
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 
-<body id="homepage">
+<body>
 <div id="left"></div>
 <div id="right"></div>
 <div id="top"></div>
@@ -17,18 +17,16 @@
 <div id="container">
   <div id="sidebar">
     <div class="logotype">
-      <h1><a href="blackburn_foster/index.html">BLACKBURN <br> and FOSTER</a></h1>
+      <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>">BLACKBURN <br> and FOSTER</a></h1>
     </div>
 
       <div class="menu">
         <ul>
           <li class="one"><a href="#">Work</a></li>
           <li class="two"><a href="#">About</a></li>
-          <li class="three"><a href="/blackburn_foster/blog.html">Blog</a></li>
+          <li class="three"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Blog</a></li>
         </ul>
       </div>
-
-    	<p>Blackburn and Foster <br>is a menswear brand focusing on durable products that build character over time.</p>
 
       <div class="social_icons">
         <a href="#" i class="ss-icon ss-social-circle">Facebook</i></a>
@@ -37,6 +35,20 @@
         <a href="#" i class="ss-icon ss-social-circle">Linkedin</i></a>
       </div>
   </div>
+</div>
+
+<div id="primary">
+      <div id="content">
+      <?php if (have_posts()) : ?>
+
+          <div class="post">
+          <?php while (have_posts()) : the_post(); ?>
+            <p class="date"><?php the_date(); ?></p>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content('Read the rest of this entry &raquo;'); ?></p>
+          <?php endwhile ?>
+        <?php endif ?>
+</div>
 </div>
 
 </body>
